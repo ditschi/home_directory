8e24c0c4a2188fafece836a6f53c78528a9bd8f7
Christian Ditscher <chris@ditscher.me>
2022-01-10 15:55:30 +0100

    moved out artifactory api-key to ~/.env (and created new one on artifactory)

--- .zsh/config/01_bosch.zsh
+++ .zsh/config/01_bosch.zsh
@@ -8,6 +8,9 @@ if [[ ! $USER =~ ^[a-zA-Z]{3}[0-9]{1,2}[a-zA-Z]{2,3}$ ]]; then
 fi
 #echo "User '$USER' matches bosch username pattern -> lodading Bosch config"
 
+
+
+
 # set the Bosch user in .gitconfig
 git config --global user.name "Ditscher Christian (XC-DA/EAS3)"
 git config --global user.email "Christian.Ditscher@de.bosch.com"
@@ -19,11 +22,19 @@ export ftp_proxy=:http://localhost:3128
 export no_proxy=localhost,127.0.0.1,*.microsoftonline.com,*.bosch.com
 
 # dev-env setup
+if [ -f ~/.env ]
+then
+    # load the secret values from ~/.env
+    source ~/.env
+    export ARTIFACTORY_API_KEY
+    export CONAN_PASSWORD
+else
+    echo "No '~/.env' file was found to load secrets from"
+fi
 export DOCKER_USER=$(whoami) && export DOCKER_UID=$(id -u) && export DOCKER_GID=$(id -g)
 export CONTAINER_USER=$(whoami) && export CONTAINER_UID=$(id -u) && export CONTAINER_GID=$(id -g)
-export ARTIFACTORY_API_KEY=AKCp8ihf8XjZaVFcgLYzTpzaCNnXbRh9DNXWLFgjcpEGxNAorNjemDBbY1pGfn7qi4qqtdXgA
+export ARTIFACTORY_API_KEY  # from ~/env file
 export CONAN_LOGIN_USERNAME=dci2lr
-export CONAN_PASSWORD=$ARTIFACTORY_API_KEY
 
 # aliases
 alias zshbosch="nano $ZSH/config/01_bosch.zsh"
